019216121e59f159a1adab2d103b33ad91f854d6
vpetrush18 <v_petrush@hotmail.com>
2015-06-02 21:08:17 +0200

    Prv Commit

---
+++ WebAplikacijaMovies/Default.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAplikacijaMovies.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            width: 33%;
+        }
+        .style2
+        {
+            text-align: justify;
+        }
+        .style3
+        {
+            text-align: center;
+        }
+    </style>
+</head>
+<body style="text-align: center">
+    <form id="form1" runat="server">
+    <div>
+    
+        <table align="center" class="style1">
+            <tr>
+                <td>
+                    <asp:ListBox ID="lstFilmovi" runat="server" AutoPostBack="True" Height="241px" 
+                        onselectedindexchanged="lstFilmovi_SelectedIndexChanged" Width="286px">
+                    </asp:ListBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblPoraka" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnNovFilm" runat="server" onclick="btnNovFilm_Click" 
+                        Text="Нов Филм" />
+&nbsp;
+                    <asp:Button ID="btnVnesi" runat="server" onclick="btnVnesi_Click" 
+                        Text="Внеси" />
+&nbsp;
+                    <asp:Button ID="btnZacuvaj" runat="server" onclick="btnZacuvaj_Click" 
+                        Text="Измени" />
+&nbsp;
+                    <asp:Button ID="btnIzbrisi" runat="server" onclick="btnIzbrisi_Click" 
+                        Text="Избриши" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="Label2" runat="server" Text="Име"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="txtIme" runat="server" Width="180px"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="Label3" runat="server" Text="Режисер"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="txtReziser" runat="server" Width="180px"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="Label7" runat="server" Text="Машка Улога"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="txtMaskaUloga" runat="server" Width="180px"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="Label8" runat="server" Text="Женска Улога"></asp:Label>
+&nbsp;&nbsp;
+                    <asp:TextBox ID="txtZenskaUloga" runat="server" Width="180px"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="Label4" runat="server" Text="Жанр"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="txtZanr" runat="server" Width="180px"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="Label5" runat="server" Text="Година"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="txtGodina" runat="server" Width="180px"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="Label6" runat="server" Text="Времетраење"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="txtVremetraenje" runat="server" Width="180px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="style2">
+                    &nbsp;</td>
+            </tr>
+        </table>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebAplikacijaMovies/Default.aspx.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebAplikacijaMovies
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                ispolniLista();
+        }
+
+        protected void ispolniLista()
+        {
+            lstFilmovi.Items.Clear();
+            SqlConnection kon = new SqlConnection();
+            kon.ConnectionString =
+                ConfigurationManager.ConnectionStrings["konekcija"].ConnectionString;
+            string sqlString = "SELECT film_id, film_ime FROM tblFilmovi";
+            SqlCommand komanda = new SqlCommand(sqlString, kon);
+
+            try
+            {
+                kon.Open();
+                SqlDataReader citac = komanda.ExecuteReader();
+
+                while (citac.Read())
+                {
+                    ListItem element = new ListItem();
+                    element.Text = citac["film_ime"].ToString();
+                    element.Value = citac["film_id"].ToString();
+                    lstFilmovi.Items.Add(element);
+                }
+                citac.Close();
+            }
+
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+
+            finally
+            {
+                kon.Close();
+            }
+        }
+
+        protected void lstFilmovi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selektirajFilm(lstFilmovi.SelectedValue);
+        }
+
+        protected void selektirajFilm(string id)
+        {
+            SqlConnection kon = new SqlConnection();
+            kon.ConnectionString =
+                ConfigurationManager.ConnectionStrings["konekcija"].ConnectionString;
+            string sqlString = "SELECT film_id, film_ime, reziser, maska_uloga, zenska_uloga, zanr, godina, vremetraenje FROM tblFilmovi WHERE film_id='" + id + "'";
+            SqlCommand komanda = new SqlCommand(sqlString, kon);
+
+            try
+            {
+                kon.Open();
+                SqlDataReader citac = komanda.ExecuteReader();
+                if (citac.Read())
+                {
+                    txtIme.Text = citac["film_ime"].ToString();
+                    txtReziser.Text = citac["reziser"].ToString();
+                    txtMaskaUloga.Text = citac["maska_uloga"].ToString();
+                    txtZenskaUloga.Text = citac["zenska_uloga"].ToString();
+                    txtZanr.Text = citac["zanr"].ToString();
+                    txtGodina.Text = citac["godina"].ToString();
+                    txtVremetraenje.Text = citac["vremetraenje"].ToString();
+                    citac.Close();
+                }
+            }
+
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+
+            finally
+            {
+                kon.Close();
+            }
+        }
+
+        protected void btnNovFilm_Click(object sender, EventArgs e)
+        {
+            txtIme.Text = "";
+            txtReziser.Text = "";
+            txtMaskaUloga.Text = "";
+            txtZenskaUloga.Text = "";
+            txtZanr.Text = "";
+            txtGodina.Text = "";
+            txtVremetraenje.Text = "";
+        }
+
+        protected void btnVnesi_Click(object sender, EventArgs e)
+        {
+            SqlConnection kon = new SqlConnection();
+            kon.ConnectionString =
+                ConfigurationManager.ConnectionStrings["konekcija"].ConnectionString;
+            SqlCommand komanda = new SqlCommand();
+            komanda.Connection = kon;
+            komanda.CommandText = "INSERT INTO tblFilmovi (film_ime, reziser, maska_uloga, zenska_uloga, zanr, godina, vremetraenje)" +
+                        "VALUES (@film_ime, @reziser, @maska_uloga, @zenska_uloga, @zanr, @godina, @vremetraenje)";
+            komanda.Parameters.AddWithValue("@film_ime", txtIme.Text);
+            komanda.Parameters.AddWithValue("@reziser", txtReziser.Text);
+            komanda.Parameters.AddWithValue("@maska_uloga", txtMaskaUloga.Text);
+            komanda.Parameters.AddWithValue("@zenska_uloga", txtZenskaUloga.Text);
+            komanda.Parameters.AddWithValue("@zanr", txtZanr.Text);
+            komanda.Parameters.AddWithValue("@godina", txtGodina.Text);
+            komanda.Parameters.AddWithValue("@vremetraenje", txtVremetraenje.Text);
+
+            try
+            {
+                kon.Open();
+                komanda.ExecuteNonQuery();
+            }
+
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+
+            finally
+            {
+                kon.Close();
+            }
+            ispolniLista();
+        }
+
+        protected void btnZacuvaj_Click(object sender, EventArgs e)
+        {
+            SqlConnection kon = new SqlConnection();
+            kon.ConnectionString =
+                ConfigurationManager.ConnectionStrings["konekcija"].ConnectionString;
+            SqlCommand komanda = new SqlCommand();
+            komanda.Connection = kon;
+            komanda.CommandText = "UPDATE tblFilmovi SET " + "film_ime='" + txtIme.Text + 
+                "', reziser='" + txtReziser.Text + "', maska_uloga='" + txtMaskaUloga.Text + 
+                "', zenska_uloga='" + txtZenskaUloga.Text + "', zanr='" + txtZanr.Text + 
+                "', godina='" + txtGodina.Text + "', vremetraenje='" + txtVremetraenje.Text + "'" + 
+                    "WHERE film_id='" + lstFilmovi.SelectedValue + "'";
+
+            try
+            {
+                kon.Open();
+                komanda.ExecuteNonQuery();
+            }
+
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+
+            finally
+            {
+                kon.Close();
+            }
+
+            ispolniLista();
+        }
+
+        protected void btnIzbrisi_Click(object sender, EventArgs e)
+        {
+
+            if (lstFilmovi.SelectedIndex != -1)
+            {
+                SqlConnection kon = new SqlConnection();
+                kon.ConnectionString =
+                    ConfigurationManager.ConnectionStrings["konekcija"].ConnectionString;
+                SqlCommand komanda = new SqlCommand();
+                komanda.Connection = kon;
+                komanda.CommandText = "DELETE FROM tblFilmovi WHERE film_id='" + lstFilmovi.SelectedValue + "'";
+
+                try
+                {
+                    kon.Open();
+                    komanda.ExecuteNonQuery();
+                }
+
+                catch (Exception err)
+                {
+                    lblPoraka.Text = err.Message;
+                }
+
+                finally
+                {
+                    kon.Close();
+                }
+
+                ispolniLista();
+            }
+        }
+
+
+
+    }
+}
